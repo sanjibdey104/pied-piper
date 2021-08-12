85479feaa9306956bbe3effb75da51242d5388d3
sanjibdey104 <sanjibdey.dey4@gmail.com>
2021-08-12 11:48:12 +0530

    Initial commit: detect file drag events

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const dropzone = document.querySelector(".drop-zone");
+
+const active = () => dropzone.classList.add("active-border");
+const inactive = () => dropzone.classList.remove("active-border");
+const prevents = (e) => e.preventDefault();
+
+const eventList = ["dragenter", "dragover", "dragleave", "drop"];
+eventList.forEach((eventName) =>
+  dropzone.addEventListener(eventName, prevents)
+);
+
+const incomingEvents = ["dragenter", "dragover"];
+incomingEvents.forEach((eventName) =>
+  dropzone.addEventListener(eventName, active)
+);
+
+const outgoingEvents = ["dragleave", "drop"];
+outgoingEvents.forEach((eventName) =>
+  dropzone.addEventListener(eventName, inactive)
+);
